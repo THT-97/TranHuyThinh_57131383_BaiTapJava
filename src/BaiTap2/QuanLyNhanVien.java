d95ff38018d6098556281748984f9c5062de6f2a
TranHuyThinh <tranhuythinh97@gmail.com>
2020-04-11 20:59:15 +0700

    Bai 2

---
+++ src/BaiTap2/QuanLyNhanVien.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap2;
+
+import BaiTap1.NhanVien;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author TranHuyThinh
+ */
+public class QuanLyNhanVien implements IQuanLy{
+    private ArrayList<NhanVien> ds;
+    
+    public QuanLyNhanVien() {
+        ds = new ArrayList<>();
+    }
+
+    @Override
+    public void them(NhanVien nv) {
+        ds.add(ds.size(), nv);
+    }
+
+    @Override
+    public void inDS() {
+        System.out.println("Ten\t\tTuoi\tLuong\tGio Lam\t\tDia Chi\t\tThuong\n");
+        ds.forEach((nv) -> {
+            System.out.println(nv.getThongTin() +"\t" + nv.tinhThuong() +"\n");
+        });
+    }
+    
+}
--- src/tranhuythinh_57131383_baitapjava/TranHuyThinh_57131383_BaiTapJava.java
+++ src/tranhuythinh_57131383_baitapjava/TranHuyThinh_57131383_BaiTapJava.java
@@ -6,6 +6,7 @@
 package tranhuythinh_57131383_baitapjava;
 
 import BaiTap1.NhanVien;
+import BaiTap2.QuanLyNhanVien;
 
 /**
  *
@@ -17,12 +18,19 @@ public class TranHuyThinh_57131383_BaiTapJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Bai 1
         NhanVien nv1 = new NhanVien("Luong Van An", 35, "dia chi 1", 11.5d, 250);
         NhanVien nv2 = new NhanVien("Nguyen Van Ba", 25, "dia chi 2", 5.5d, 99);
         
         System.out.println("Ten\t\tTuoi\tLuong\tGio Lam\t\tDia Chi\t\tThuong\n");
         System.out.println(nv1.getThongTin() +"\t" + nv1.tinhThuong() +"\n");
         System.out.println(nv2.getThongTin() +"\t" + nv2.tinhThuong() +"\n");
+        
+        //Bai 2
+        QuanLyNhanVien ql = new QuanLyNhanVien();
+        ql.them(nv1);
+        ql.them(nv2);
+        ql.inDS();
     }
     
 }
